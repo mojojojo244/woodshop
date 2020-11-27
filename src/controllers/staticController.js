173ad3dbc15d0a13460cf59dc224f31dc5381c52
Jordan Crutcher <jordan.crutcher@gmail.com>
2020-11-26 20:59:56 -0600

    thursday

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,5 +1,11 @@
 module.exports = {
     index(req, res, next){
         res.render("static/index", {title: "Crutcher Woodcrafts"});
-    }
+    },
+    about(req, res, next) {
+        res.render("static/about", {title: "About Us"});
+    },
+    products(req, res, next) {
+        res.render("static/products", {title: "Products"});   
+    },
 }
--- src/routes/static.js
+++ src/routes/static.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const staticController = require("../controllers/staticController");
 
 router.get("/", staticController.index);
+router.get("/about", staticController.about);
+router.get("/products", staticController.products);
 
 module.exports = router;
